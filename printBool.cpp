14adcaaca40c18afe4233b5920fc527cdfc6266e
Mayank Raj <me.rajmayank@gmail.com>
2025-07-10 01:16:31 +0530

    update file name

---
+++ printBool.cpp
@@ -0,0 +1,14 @@
+//WAP to check bool number.
+#include<iostream>
+using namespace std;
+int main()
+{
+    bool number;
+
+    cout << "Enter your boolNumber: ";
+    cin >> number ;
+    cout << "Your boolNumber is:" << number << endl;
+
+    return 0;
+
+}
