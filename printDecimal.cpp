77b25a6fdada321fbcb669bf2800e271ec26dafa
Mayank Raj <me.rajmayank@gmail.com>
2025-07-09 17:34:51 +0530

    print a Decimal number

---
+++ printDecimal.cpp
@@ -0,0 +1,11 @@
+// WAP to print a Decimal number 
+#include<iostream>
+using namespace std;
+int main()
+{
+    float numberWithDecimal;
+    cout << " Enter your number " ;
+    cin >>   numberWithDecimal ;
+    cout << " your number is " <<  numberWithDecimal << endl;
+    return 0;
+}
