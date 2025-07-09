1183ab2344cf7ba7ca89e043f5dae91e24be6d6e
Mayank Raj <me.rajmayank@gmail.com>
2025-07-10 01:33:11 +0530

    hellow with character

---
+++ greetAChar.cpp
@@ -0,0 +1,13 @@
+// WAP to print 'hello' followed by a single character. 
+#include<iostream>
+using namespace std;
+int main()
+{
+    char myCharacter;
+    cout << "enter your character:";
+    cin >>myCharacter;
+    cout << "hello "<< myCharacter << endl;
+
+    return 0;
+}
+
