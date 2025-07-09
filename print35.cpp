008f4081392f4c66523c1f71695b70275e8e3c14
Mayank Raj <me.rajmayank@gmail.com>
2025-07-09 17:03:14 +0530

    print your name with an integer

---
+++ print35.cpp
@@ -0,0 +1,12 @@
+//WAP to print your Name with an integer, like- Hi John, this is your number 55.
+#include<iostream>
+using namespace std;
+int main()
+{
+   int number;
+    cout << "enter your number: " << endl;
+    cin >> number;
+    cout << "Hi Mayank ";
+    cout << number << " this is your number." << endl;
+    return 0; 
+}
