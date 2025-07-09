6454e774755eef9b88ad3a914f35edfbcdad323e
Mayank Raj <me.rajmayank@gmail.com>
2025-07-09 17:24:52 +0530

    print a character

---
+++ printChar.cpp
@@ -0,0 +1,12 @@
+// WAP to print a single character in CPP
+#include<iostream>
+using namespace std;
+int main()
+{
+    char singleVariable ;
+    cout << "Enter your Variable: ";
+    cin >> singleVariable ;
+    cout << "your Variable is : " << singleVariable << endl;
+    return 0; 
+}
+
