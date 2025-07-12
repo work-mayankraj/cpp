50d394c0a5c79f6ae051ccf56e33e292993c5024
Mayank Raj <me.rajmayank@gmail.com>
2025-07-12 23:04:35 +0530

    area of circle.

---
+++ areaOfCircle.cpp
@@ -0,0 +1,12 @@
+//Take input of radius of a circle and calculate area = 3.14 × radius × radius.
+#include<iostream>
+using namespace std;
+int main(){
+    int radius;
+    cout << "Enter the radius : ";
+    cin >> radius;
+    float area;
+    area = 3.14 *radius*radius;
+    cout << "The area of your desird circle is : " << area << endl;
+    return 0;
+}
