9fad1d80533986f2b83d3b08479cc1ae52d5bc83
Mayank Raj <me.rajmayank@gmail.com>
2025-07-13 21:21:14 +0530

    check positive,negative and zero.

---
+++ checkPositivenegative.cpp
@@ -0,0 +1,24 @@
+//WAP to check if a number is positive, negative, or zero.
+#include<iostream>
+using namespace std;
+int main(){
+    float number;
+    cout << "Enter your number :";
+    cin >> number;
+
+      if(number>0)
+      {
+        cout << "your number is positive." << endl;
+      }
+      else if(number==0)
+      {
+        cout << "your number is zero ." << endl;
+      }
+      else
+      {
+        cout << "your number is negative." << endl ;
+      }
+    
+    return 0;
+
+}
