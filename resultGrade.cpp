442eb8ab9c88f820ea32ef62a17f9504136b59e2
Mayank Raj <me.rajmayank@gmail.com>
2025-07-13 21:26:20 +0530

    marks of 3 sub, total, percentage, grade.

---
+++ resultGrade.cpp
@@ -0,0 +1,49 @@
+/*WAP that take marks in 3 subjects and print the total, percentage, and grade (A if >90, B if >80, C if >70, 
+D if >60, and Fail <=60). */
+#include<iostream>
+using namespace std;
+int main()
+{
+    float number1;
+    cout << "Enter the marks of your first subject :";
+    cin >> number1;
+
+    float number2;
+    cout << "Enter the marks of your second subject :";
+    cin >> number2;
+
+    float number3;
+    cout << "Enter the marks of your third subject :";
+    cin >> number3;
+
+    float totalMarks;
+    totalMarks = number1 + number2 + number3 ;
+    cout << "your total marks is: " << totalMarks << endl;
+
+    float percentage;
+    percentage = totalMarks /3;
+    cout << "your percentage is: " << percentage << endl;
+    
+    char grade;
+    if(percentage>90)
+    {
+        cout << "grade 'A'" << endl;
+    }
+    else if(percentage>80)
+        {
+        cout << "grade 'B'" << endl;
+        }
+        else if(percentage>70)
+        {
+        cout << "grade 'c'" << endl;
+        }
+        else if(percentage>60)
+        {
+        cout << "grade 'D'" << endl;
+        }
+    else
+    {
+    cout << "You are fail." << endl;
+    }
+    return 0;
+}
