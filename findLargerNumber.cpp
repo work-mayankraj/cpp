0db9ae4952b2bd03198d4242cf5c41ada8e48db7
Mayank Raj <me.rajmayank@gmail.com>
2025-07-13 21:24:16 +0530

    comparision between two numbers.

---
+++ findLargerNumber.cpp
@@ -0,0 +1,23 @@
+//WAP that take two float numbers and print the larger number.
+#include<iostream>
+using namespace std;
+int main()
+    {
+    float number1;
+    cout << "Enter your first number :";
+    cin >> number1;
+
+    float number2;
+    cout << "Enter your second number :";
+    cin >> number2;
+
+    if(number1 > number2){
+        cout<< number1 << " > "  << number2 << endl;
+    }
+    else 
+    {
+        cout << number1 << " > " << number2 << endl;
+    }
+
+    return 0;
+}
