c383263686ae611b5fc7d744d289345d135b1c88
Mayank Raj <me.rajmayank@gmail.com>
2025-07-18 23:51:06 +0530

    count money

---
+++ countMoney.cpp
@@ -0,0 +1,51 @@
+/* WAP that takes a number as input an amount (like ₹587) and display how many 100, 50, 20, 10, 5, 2 and 1 rupee 
+notes it needs.*/
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num1;
+    cout << "Enter your number: ";
+    cin >> num1;
+ 
+    int hund;
+    hund=num1/100;
+
+    int num2;
+    num2=hund%100;
+
+    int fifty;
+    fifty=num2/50;
+
+    int num3;
+    num3=num2%50;
+
+    int twenty;
+    twenty= num3/20;
+
+    int num4;
+    num4=num3%20;
+
+    int ten;
+    ten=num4/10;
+
+    int num5;
+    num5=num4%10;
+
+    int five;
+    five=num5/5;
+
+    int num6;
+    num6=num5%5;
+
+    int two;
+    two=num6/2;
+
+    int num7;
+    num7=num6%2;
+
+    int one;
+    one=num7/1;
+
+    cout << hund << fifty<<twenty<<ten<<five<<two<<one <<endl;
+}
