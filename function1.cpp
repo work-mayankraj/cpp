6619080b9fbada68847b7e9007664dbee7f4b728
Mayank Raj <me.rajmayank@gmail.com>
2025-07-20 23:00:29 +0530

    function hello world

---
+++ function1.cpp
@@ -0,0 +1,13 @@
+//WAP to create a function and print "Hello World".
+#include<iostream>
+using namespace std;
+
+void myFunction()
+{
+    cout << "Hello World" << endl;
+}
+    int main()
+{
+    myFunction();
+    return 0;
+}
