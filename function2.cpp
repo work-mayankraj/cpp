04ee6f1a942ee2710b727151f361ff58250d5c45
work-mayankraj <me.rajmayank@gmail.com>
2025-07-20 22:37:57 +0530

    Create function2.cpp

---
+++ function2.cpp
@@ -0,0 +1,13 @@
+/*WAP to take a number as input and the number then print hello world.*/
+#include <iostream>
+
+using namespace std;
+
+void myFun(){
+	int num=5;
+	cout <<num << " Hello World" << endl;
+}
+int main(){
+	myFun();
+	return 0;
+	}
